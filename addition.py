75fbf3c6f35b4af2491879f1c60a6eada4817ba1
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-19 20:00:03 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,12 +1,10 @@
-
+Q1)How to add two integers by using python programming?
+A:By writing the following program in sublime text:
 a=43
 b=32
 #add a and b
 c=a+b
 print(a,"+",b,"=",c)
 
-
-
-python addition.py
+Output of above program:
 (43, '+', 32, '=', 75)
-
