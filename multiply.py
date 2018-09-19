f9014580b6e4d522585a682244f56922e87277c6
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-19 20:23:58 +0530

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,6 +1,7 @@
+Q4)How to use relational operations in python programming?
+A:By writing the following program in sublime text:
 a=input("enter value for a:")
 b=input("enter value for b:")
-
 if(a>b):
     print(a,"is greater than",b)
 elif(a==b):
@@ -8,10 +9,7 @@ elif(a==b):
 elif(b>a):
 	print(b,"is greater than",a)
 
-
-
-python multiply.py
+Output of the above program:
 enter value for a:1000
 enter value for b:2000
 (2000, 'is greater than', 1000)
-
