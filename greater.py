c4292a3cb9019d3b9e1e2ab47c55bba796b59a86
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-19 20:06:17 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,3 +1,4 @@
+Q1)How to use relational operations in python programming?
 a=input("enter value of a:")
 b=input("enter value of b:")
 c=input("enter value of c:")
@@ -7,14 +8,12 @@ elif(b>c):
   print(b,"is greater than")
 else:
   print(c,"is greater than")
-
-
- python greater.py
+Output of the above program:
 enter value of a:23
 enter value of b:7
 enter value of c:8
 (23, 'is greater than')
-dl210@soetcse:~/varshini$ python greater.py
+
 enter value of a:45
 enter value of b:100
 enter value of c:200
