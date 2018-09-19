089f026204fa3f29ad19e8e6e7dba3dfec446a71
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-19 20:48:15 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,3 +1,5 @@
+Q6)How to use relational operations in python programming?
+A:By writing the following program in sublime text:
 a=input("enter value of a")
 b=input("enter value of b")
 if(a>b):
@@ -5,10 +7,7 @@ if(a>b):
 elif(b>a):
      print(b,"is positive")  
 
-
-
-  python positive.py
+  Output of the above program:
 enter value of a500
 enter value of b1000
 (1000, 'is positive')
-
