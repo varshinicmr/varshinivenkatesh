da7eb8b5d34039499a183cab0c13f1b4f6253317
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-19 20:16:04 +0530

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -1,9 +1,9 @@
+Q3)How to communicate using python programming?
+A:By writing the following in sublime text:
 #write a program to ask the user her/him name
 #greet the use using that name
 #e.g: Hello,Varsha,how are you?
 print("hello,")
 
-
-python introduction.py
+Output of above program:
 hello,
-
