02e10b97767058423d5726302b3500272fa0ed17
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-20 19:28:00 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -33,3 +33,14 @@ enter value of a273
 enter value of b365
 (273, '*', 365, '=', 99645)
 
+D)DIVISION:
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+#add a and b
+c=a/b
+print(a,"/",b,"=",c)
+
+Output of above program:
+enter value of a555
+enter value of b30
+(555, '/', 30, '=', 18)
