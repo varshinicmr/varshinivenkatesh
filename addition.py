977a0bd0c05cf399c98737a30c94fde75d48b484
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-20 19:19:21 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -8,3 +8,15 @@ print(a,"+",b,"=",c)
 
 Output of above program:
 (43, '+', 32, '=', 75)
+
+B)SUBTRACTION:
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+#subtract a and b
+c=a-b
+print(a,"-",b,"=",c)
+
+Output of above program:
+enter value of a433
+enter value of b555
+(433, '-', 555, '=', -122)
