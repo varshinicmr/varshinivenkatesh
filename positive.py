060cf6c5d6b8e5297ec683c69f18f48778e47b4b
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-20 19:49:46 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -11,3 +11,17 @@ elif(b>a):
 enter value of a500
 enter value of b1000
 (1000, 'is positive')
+
+7. CHECKING WHETHER THE NUMBER IS NEGATIVE OR NOT:
+     
+a=input("enter value of a")
+b=input("enter the value of b")
+if(a>b and a>c):
+     print(a,"is negative")
+elif(b>a):
+     print(b,"is negative")
+
+Output of the above program:
+enter value of a200
+enter the value of b400
+(400, 'is negative')
