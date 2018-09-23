a23c13ff9db046bed5613419ec1c60c69dd21826
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-23 15:14:08 +0530

    Update import.py

--- import.py
+++ import.py
@@ -1,10 +1,28 @@
-RANDOM FUNCTION:
+18. RELATIONAL OPERATORS:
+  
+>>>
+3**3
+      27
+      >>>4 or 5
+      4
+      >>>4 and 5
+      5
+      >>>9<8
+      False
+      >>>6>4
+      True
+      >>>6==6
+      True
+      >>>6!=7
+      True
+      
+19. RANDOM FUNCTION:
   
 import random
 r=random.randint(1,6)
 print(r)
 
-OUTPUT:
+Output of above program:
 3
 
 6
