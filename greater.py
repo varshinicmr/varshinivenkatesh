1473af032123a8e85cf5885af3f51b6f045ba3ac
varshinicmr <43023426+varshinicmr@users.noreply.github.com>
2018-09-23 14:30:53 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,3 +1,20 @@
+4.COMPARING TWO NUMBERS:
+	
+a=input("enter value for a:")
+b=input("enter value for b:")
+if(a>b):
+    print(a,"is greater than",b)
+elif(a==b):
+    print(a,"is equal to",b)
+elif(b>a):
+	print(b,"is greater than",a)
+
+Output of the above program:
+enter value for a:1000
+enter value for b:2000
+(2000, 'is greater than', 1000)
+
+
 5.COMPARING THREE NUMBERS:
 
 #comparing 3 no.s
